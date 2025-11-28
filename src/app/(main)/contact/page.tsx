5ac963680e8a01564629613dcd18cbc8f8e349df
Hafis TFG <hafiscp.tfg@gmail.com>
2025-11-28 08:44:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -63,12 +63,10 @@ export default function ContactPage() {
                                         return <a key={index} href={line.href} className="text-slate-600 text-base font-normal leading-relaxed mt-1 hover:text-primary transition-colors">{line.text}</a>
                                     })}
                                     {item.button && (
-                                        <Link href={item.button.href} passHref legacyBehavior>
-                                            <Button asChild className="mt-4 w-fit">
-                                                <a>
-                                                    <item.button.icon className="mr-2" />
-                                                    {item.button.text}
-                                                </a>
+                                        <Link href={item.button.href}>
+                                            <Button className="mt-4 w-fit">
+                                                <item.button.icon className="mr-2" />
+                                                {item.button.text}
                                             </Button>
                                         </Link>
                                     )}
